9ebefbac8f0ba8a9a12ebf512e069c451ad2c00c
ryosukes <nu.ciger@gmail.com>
2017-05-01 20:23:31 +0900

    add index handler

--- server.go
+++ server.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/ryosukes/engineer-prof/handler"
 )
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
-	})
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/", index.Show())
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
